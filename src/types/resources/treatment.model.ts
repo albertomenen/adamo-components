abb5e3f2120b497c6533be868c92fce6d86549d1
Isidro Gonzalez <igonzalezprs@gmail.com>
2021-09-02 20:03:55 -0400

    Agregar Create en el treatment

--- src/types/resources/treatment.model.ts
+++ src/types/resources/treatment.model.ts
@@ -60,6 +60,44 @@ export interface Treatment {
   image_thermic_data: string | null
 }
 
+/**
+ * Tratamientos de un paciente
+ */
+export interface TreatmentCreate {
+  name: string
+  sessions_number: number
+  notes: string
+  temperature: number
+  heating_duration: number
+  points: string
+  ts_end: number
+  weight: number
+  height: number
+  ppx: number
+  ppy: number
+  fx: number
+  fy: number
+  model: string
+  coeff: string
+  depth_scale: number
+  mode: string
+  extrinsics: number
+  move: string
+  sessions: Session[]
+  type: TreatmentTypes
+  state: TreatmentStatus
+  last_session_date: Date | string
+  injury: boolean
+  injury_kind: string
+  injury_cause: string
+  image_3D: string | null
+  image_thermic: string | null
+  image_thermic_width: number | null
+  image_thermic_height: number | null
+  image_thermic_depth: number | null
+  image_thermic_data: string | null
+}
+
 export interface TreatmentList {
   id_treatment: string
   medic_name: string
