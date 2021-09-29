f1a2a640a5a13a83321f57ec1c6bc0544f6db8e0
Isidro Gonzalez <igonzalezprs@gmail.com>
2021-09-29 19:13:30 -0400

    Quitar password del formulario de registro

--- src/components/forms/user-register-form/AFormUserRegister.vue
+++ src/components/forms/user-register-form/AFormUserRegister.vue
@@ -15,28 +15,6 @@
       icon-pack="far"
       :placeholder="$t('fields.email')"
     />
-    <AInput
-      v-model="formData.password"
-      class="column is-full"
-      icon-pack="fas"
-      icon-right="eye-slash"
-      password-reveal
-      :placeholder="$t('fields.password')"
-      type="password"
-    />
-    <div class="column is-full has-text-centered pt-0">
-      <p class="pass-composition">
-        Mínimo 8 caracteres, mayúsculas, minúsculas, números y símbolos.
-      </p>
-    </div>
-    <AInput
-      class="column is-full"
-      icon-pack="fas"
-      icon-right="eye-slash"
-      password-reveal
-      :placeholder="$t('fields.password')"
-      type="password"
-    />
   </div>
 </section>
 </template>
@@ -44,10 +22,6 @@
 <script src="./AFormUserRegister.ts" lang="ts"></script>
 
 <style lang="scss">
-.pass-composition {
-  font-size: 14px;
-  color: #7C7C7C;
-}
 .icon.is-right.is-clickable {
   color: #BCBCBC !important;
 }
