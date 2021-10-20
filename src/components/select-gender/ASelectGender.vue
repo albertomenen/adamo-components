e71dd2e58a8e375e047f9bbd07eed158115701ad
Isidro Gonzalez <igonzalezprs@gmail.com>
2021-10-20 14:36:18 -0400

    Wrapper de Field

--- src/components/select-gender/ASelectGender.vue
+++ src/components/select-gender/ASelectGender.vue
@@ -1,11 +1,13 @@
 <template>
-<BSelect
-  v-bind="$attrs"
-  expanded
-  rounded
-  v-on="$listeners">
-  <option value="male">Hombre</option>
-  <option value="female">Mujer</option>
-  <option value="other">Otro</option>
-</BSelect>
+<BField>
+  <BSelect
+    v-bind="$attrs"
+    expanded
+    rounded
+    v-on="$listeners">
+    <option value="male">Hombre</option>
+    <option value="female">Mujer</option>
+    <option value="other">Otro</option>
+  </BSelect>
+</BField>
 </template>
--- src/components/select-race/ASelectRace.vue
+++ src/components/select-race/ASelectRace.vue
@@ -1,13 +1,15 @@
 <template>
-<BSelect
-  v-bind="$attrs"
-  expanded
-  rounded
-  v-on="$listeners">
-  <option value="asian">Asiático</option>
-  <option value="american">Americano</option>
-  <option value="caucasian">Caucásico</option>
-  <option value="malayan">Malayo/Pardo</option>
-  <option value="black">Etiópico</option>
-</BSelect>
+<BField>
+  <BSelect
+    v-bind="$attrs"
+    expanded
+    rounded
+    v-on="$listeners">
+    <option value="asian">Asiático</option>
+    <option value="american">Americano</option>
+    <option value="caucasian">Caucásico</option>
+    <option value="malayan">Malayo/Pardo</option>
+    <option value="black">Etiópico</option>
+  </BSelect>
+</BField>
 </template>
